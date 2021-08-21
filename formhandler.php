7b2106032e4489720ed5f43de6a485f8a6d55f71
MacOS host <69931086+noisehunter82@users.noreply.github.com>
2021-08-21 14:48:39 +0100

    Fix empty attamchent bug

--- formhandler.php
+++ formhandler.php
@@ -56,11 +56,11 @@ if (isset($_POST) && !empty($_POST)) {
 
   $validatedData;
 
-  foreach($_POST as $key => $value) {
-    $validatedData[$key] = validateInput($key,$value);
+  foreach ($_POST as $key => $value) {
+    $validatedData[$key] = validateInput($key, $value);
   }
 
-    
+
   $firstName = $validatedData['first-name'];
   $lastName = $validatedData['last-name'];
   $email = $validatedData['email'];
@@ -94,9 +94,9 @@ if (isset($_POST) && !empty($_POST)) {
   <p>Postcode: <span>' . $postcode . '</span></p>
  <p>Description: <span>' . $description . '</span></p>';
 
-  
- // Send email without attachment
-  if (!isset($_FILES['cv']) || $_FILES['cv']['error'] != 0 || !isCorrectFormat($_FILES['cv']['name'])) {
+
+  // Send email without attachment
+  if (!is_uploaded_file($_FILES['cv']['tmp_name']) || $_FILES['cv']['error'] != 0 || !isCorrectFormat($_FILES['cv']['name'])) {
 
     $headers[] = 'MIME-Version: 1.0';
     $headers[] = 'Content-type:text/html;charset=utf-8';
@@ -113,7 +113,6 @@ if (isset($_POST) && !empty($_POST)) {
 
       echo json_encode($output);
       exit;
-
     } else {
 
       $output['status']['code'] = '500';
@@ -126,24 +125,17 @@ if (isset($_POST) && !empty($_POST)) {
       echo json_encode($output);
       exit;
     }
-
-  
   } else {
     //Send email with attachment
 
+    $fileName = $_FILES['cv']['name'];
+    $tempPath = $_FILES['cv']['tmp_name'];
+    $fileType = $_FILES['cv']['type'];
+    $fileSize = $_FILES['cv']['size'];
 
-    $file_name = $_FILES['cv']['name'];
-    $temp_name = $_FILES['cv']['tmp_name'];
-    $file_type = $_FILES['cv']['type'];
-    $file_size = $_FILES['cv']['size'];
+    $base = basename($fileName);
 
-    $base = basename($file_name);
-
-
-    $file = fopen($temp_name, 'rb');
-    $data = fread($file, filesize($temp_name));
-    fclose($file);
-    $encoded_data = chunk_split(base64_encode($data));
+    $encoded_data = chunk_split(base64_encode(file_get_contents($tempPath)));
 
     $mime_boundary = md5(time());  //unique identifier
 
@@ -151,9 +143,8 @@ if (isset($_POST) && !empty($_POST)) {
     $headers[] = 'From: FormTask<formtask@example.com>';
     $headers[] = 'Reply-To: FormTask<formtask@example.com>';
     $headers[] = 'MIME-Version: 1.0';
-
-    //declare multiple kinds of email (plain text + attch)
     $headers[] = 'Content-Type: multipart/mixed; boundary="' . $mime_boundary;
+    $headers[] = 'This is a MIME encoded message.';
 
     //message part
     $messages[] = '--' . $mime_boundary;
@@ -161,14 +152,13 @@ if (isset($_POST) && !empty($_POST)) {
     $messages[] = 'Content-Transfer-Encoding:7bit';
     $messages[] = $htmlMessage;
 
-
     //attch part
     $messages[] = '--' . $mime_boundary;
-    $messages[] = 'Content-Type: ' . $file_type . ';name=' . $file_name;
+    $messages[] = 'Content-Type:' . $fileType .  ';name="' . $base . '"';
     $messages[] = 'Content-Transfer-Encoding: base64';
-    $messages[] = 'Content-Disposition: attachment;filename=' . $file_name;
+    $messages[] = 'Content-Disposition:attachment;filename="' . $base . '";size ="' . $fileSize . '"' . "\r\n";
     $messages[] = $encoded_data . "\r\n";
-    $messages[] = '--' . $mime_boundary . "--\r\n";
+    $messages[] = '--' . $mime_boundary . "--";
 
 
 
@@ -176,14 +166,13 @@ if (isset($_POST) && !empty($_POST)) {
 
       $output['status']['code'] = '200';
       $output['status']['name'] = 'ok';
-      $output['status']['description'] = 'Success! Form data received and emailed to' . $email;
+      $output['status']['description'] = 'Success! Form data received and emailed to ' . $email;
       $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . ' ms';
 
       header('Content-Type: application/json; charset=UTF-8');
 
       echo json_encode($output);
       exit;
-
     } else {
 
       $output['status']['code'] = '500';
@@ -195,7 +184,6 @@ if (isset($_POST) && !empty($_POST)) {
 
       echo json_encode($output);
       exit;
-
     }
   }
 }
